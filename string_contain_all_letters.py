7a55e0308e1dd51abed53d9d29eec8958d795cfd
ram <ramakrishnagmdo2@gmail.com>
2024-09-11 14:22:53 +0530

    string conatin all letters

---
+++ string_contain_all_letters.py
@@ -0,0 +1,23 @@
+
+alphabets = "abcdEFI jklmNop"
+new = alphabets.replace(" ", "")
+
+print(new)
+lower = alphabets.lower()
+
+print(lower)
+all_alha = lower.isalpha()\
+
+print(all_alha)
+
+# isalpha
+# isallnum
+#isdecimal
+#isnumeric
+#ascii_lowercase
+#import string 
+name = " ramakr ishna "
+new = name.strip()
+#middle = name.remove(" ") only list has remove method
+print(new)
+print(middle)
